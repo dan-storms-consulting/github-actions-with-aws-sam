afa48368dd67312b4a00f58c5f3443510eb38163
dstorms101 <48081352+dstorms101@users.noreply.github.com>
2022-02-04 05:20:32 +0800

    Update app.py
    lex | lamda connection works

--- pt-chatbot/app.py
+++ pt-chatbot/app.py
@@ -6,15 +6,20 @@ logger.setLevel(logging.DEBUG)
 
 
 def lambda_handler(event, context):
-    logger.debug(event)
-    logger.debug(event["sessionState"]["intent"])
-    logger.debug(event["sessionState"]["intent"]["slots"])
+    # logger.debug(event)
+    # logger.debug(event["sessionState"]["intent"])
+    # logger.debug(event["sessionState"]["intent"]["slots"])
 
     intent_name = event["sessionState"]["intent"]["name"]
 
     if intent_name == "GetContactInfoIntent":
-        return parse_get_contact_info_intent(event, intent_name)
-    # elif (add more intents here for multi-intent bots)
+        response_parameters = parse_get_contact_info_intent(event, intent_name)
+
+        message = response_parameters[0]
+        fulfillment_state = response_parameters[1]
+
+        return close(intent_name, fulfillment_state, message)
+
     else:
         raise Exception("Intent with name %s not supported" % intent_name)
 
@@ -25,20 +30,37 @@ def parse_get_contact_info_intent(event, intent_name):  # rename function
 
     try:  # TODO need to figure out how None is based from Lex
         # Required fields
+
+        # dict format {'first_name_slot': {'originalValue': 'dan', 'interpretedValue': 'dan', 'resolvedValues': []},
+
         try:
-            first_name_slot = slots.get("FirstNameSlot")
-            last_name_slot = slots.get("LastNameSlot")
-            email_slot = slots.get("EmailSlot")
-            phone_slot = slots.get("PhoneSlot")
+            first_name_interpreted = (
+                slots.get("FirstNameSlot").get("value").get("interpretedValue")
+            )
+            last_name_interpreted = (
+                slots.get("LastNameSlot").get("value").get("interpretedValue")
+            )
+            email_interpreted = (
+                slots.get("EmailSlot").get("value").get("interpretedValue")
+            )
+            phone_interpreted = (
+                slots.get("PhoneSlot").get("value").get("interpretedValue")
+            )
         except:
             raise Exception("Required field not provided")
 
         # Optional fields
         try:
             # TODO might need if None
-            insurance_company_slot = slots.get("InsuranceCompanySlot")
-            insurance_number_slot = slots.get("InsuranceNumberSlot")
-            date_of_birth_slot = slots.get("DateOfBirthSlot")
+            insurance_company_interpreted = (
+                slots.get("InsuranceCompanySlot").get("value").get("interpretedValue")
+            )
+            insurance_number_interpreted = (
+                slots.get("InsuranceNumberSlot").get("value").get("interpretedValue")
+            )
+            date_of_birth_interpreted = (
+                slots.get("DateOfBirthSlot").get("value").get("interpretedValue")
+            )
         except:
             raise Exception(
                 "is this really an exception?"
@@ -46,13 +68,13 @@ def parse_get_contact_info_intent(event, intent_name):  # rename function
 
         # dict to return
         contact_info = {
-            "first_name_slot": first_name_slot,
-            "last_name_slot": last_name_slot,
-            "email_slot": email_slot,
-            "phone_slot": phone_slot,
-            "insurance_company_slot": insurance_company_slot,
-            "insurance_number_slot": insurance_number_slot,
-            "date_of_birth_slot": date_of_birth_slot,
+            "first_name_interpreted": first_name_interpreted,
+            "last_name_interpreted": last_name_interpreted,
+            "email_interpreted": email_interpreted,
+            "phone_interpreted": phone_interpreted,
+            "insurance_company_interpreted": insurance_company_interpreted,
+            "insurance_number_interpreted": insurance_number_interpreted,
+            "date_of_birth_interpreted": date_of_birth_interpreted,
         }
 
         logger.debug(contact_info)
@@ -60,36 +82,15 @@ def parse_get_contact_info_intent(event, intent_name):  # rename function
         # TODO call notification function
         confirmation_number = send_email(contact_info)
 
-        message = (
-            "Thanks, I have let our staff know and they will reach out to you shortly. Your confirmation number is "
-            + str(confirmation_number)
-        )
+        # f'Hello, {name}!'
+        message = f"Thanks, I have let our staff know and they will reach out to you shortly. Your confirmation number is {confirmation_number}"  # TODO str format
         fulfillment_state = "Fulfilled"
+
+        return message, fulfillment_state
 
     except:
         raise Exception("tbd")
         logger.debug("tbd")
-
-    # return close(
-    #     intent_name,
-    #     fulfillment_state,
-    #     message,
-    # )
-
-
-# def close(intent_name, fulfillment_state, message):
-#     response = {
-#         "sessionState": {
-#             "dialogAction": {"type": "Close"},
-#             "intent": {
-#                 "confirmationState": "Confirmed",
-#                 "name": "get-info",
-#                 "state": "Fulfilled",
-#             },
-#         }
-#     }
-#     #return response
-#     #return intent_name
 
 
 def send_email(contact_info):
@@ -97,15 +98,26 @@ def send_email(contact_info):
     # TODO SES stuff
     confirmation_number = "A12345"
 
+    # send contact_info
+
     return confirmation_number
 
-    # return {
-    #     "statusCode": 200,
-    #     "body": json.dumps(
-    #         {
-    #             "intent_name": event
-    #             # "message": "hello world",
-    #             # "location": ip.text.replace("\n", "")
-    #         }
-    #     ),
-    # }
+
+def close(intent_name, fulfillment_state, message):
+    response = {
+        "sessionState": {
+            "dialogAction": {"type": "Close"},
+            "intent": {
+                "confirmationState": "Confirmed",
+                "name": intent_name,
+                "state": "Fulfilled",
+            },
+        },
+        "messages": [
+            {
+                "contentType": "PlainText",
+                "content": message,
+            }
+        ],
+    }
+    return response
