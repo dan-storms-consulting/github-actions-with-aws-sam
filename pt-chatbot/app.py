6259d9fab34118901781f510bbad2ff50f123e53
dstorms101 <48081352+dstorms101@users.noreply.github.com>
2022-02-04 11:42:03 +0800

    Update app.py

--- pt-chatbot/app.py
+++ pt-chatbot/app.py
@@ -98,7 +98,96 @@ def send_email(contact_info):
     # TODO SES stuff
     confirmation_number = "A12345"
 
-    # send contact_info
+    import boto3
+    from botocore.exceptions import ClientError
+
+    # Replace sender@example.com with your "From" address.
+    # This address must be verified with Amazon SES.
+    SENDER = "Sender Name <pt.chatbot.dev@danstormsconsulting.io>"  # TODO env var
+
+    # Replace recipient@example.com with a "To" address. If your account
+    # is still in the sandbox, this address must be verified.
+    RECIPIENT = "pt.chatbot.dev@danstormsconsulting.io"  # TODO env var
+
+    # Specify a configuration set. If you do not want to use a configuration
+    # set, comment the following variable, and the
+    # ConfigurationSetName=CONFIGURATION_SET argument below.
+    CONFIGURATION_SET = "ConfigSet"  # TODO could be useful for analytics https://docs.aws.amazon.com/ses/latest/dg/using-configuration-sets.html
+
+    # If necessary, replace us-west-2 with the AWS Region you're using for Amazon SES.
+    AWS_REGION = "us-east-1"
+
+    # The subject line for the email.
+    SUBJECT = "PT-Chatbot Test"
+
+    # The email body for recipients with non-HTML email clients.
+    BODY_TEXT = (
+        "PT-Chatbot Test\r\n"
+        "This email was sent with Amazon SES using the "
+        "AWS SDK for Python (Boto).\r\n" + contact_info
+    )
+
+    # The HTML body of the email.
+    BODY_HTML = (
+        """<html>
+    <head></head>
+    <body>
+    <h1>PT-Chatbot Test</h1>
+    <p>This email was sent with
+        <a href='https://aws.amazon.com/ses/'>Amazon SES</a> using the
+        <a href='https://aws.amazon.com/sdk-for-python/'>
+        AWS SDK for Python (Boto) """
+        + contact_info
+        + """</a>.</p>
+    </body>
+    </html>
+                """
+    )
+
+    # The character encoding for the email.
+    CHARSET = "UTF-8"
+
+    # Create a new SES resource and specify a region.
+    client = boto3.client("ses", region_name=AWS_REGION)
+
+    # Try to send the email.
+    try:
+        # Provide the contents of the email.
+        response = client.send_email(
+            Destination={
+                "ToAddresses": [
+                    RECIPIENT,
+                ],
+            },
+            Message={
+                "Body": {
+                    "Html": {
+                        "Charset": CHARSET,
+                        "Data": BODY_HTML,
+                    },
+                    "Text": {
+                        "Charset": CHARSET,
+                        "Data": BODY_TEXT,
+                    },
+                },
+                "Subject": {
+                    "Charset": CHARSET,
+                    "Data": SUBJECT,
+                },
+            },
+            Source=SENDER,
+            # If you are not using a configuration set, comment or delete the
+            # following line
+            ConfigurationSetName=CONFIGURATION_SET,
+        )
+    # Display an error if something goes wrong.
+    except ClientError as e:
+        print(e.response["Error"]["Message"])
+    else:
+        print("Email sent! Message ID:"),
+        print(response["MessageId"])
+        confirmation_number = response["MessageId"]
+        logger.debug(confirmation_number)
 
     return confirmation_number
 
