63f67872f849bde5e2a9cacfc32b19036e7fc89c
dstorms101 <48081352+dstorms101@users.noreply.github.com>
2022-02-04 12:36:40 +0800

    SES POC

--- pt-chatbot/app.py
+++ pt-chatbot/app.py
@@ -102,19 +102,22 @@ def send_email(contact_info):
 
     import boto3
     from botocore.exceptions import ClientError
+    import os
 
-    # Replace sender@example.com with your "From" address.
-    # This address must be verified with Amazon SES.
-    SENDER = "Sender Name <pt.chatbot.dev@danstormsconsulting.io>"  # TODO env var
+    sender_email = os.environ[
+        "SENDER"
+    ]  # This address must be verified with Amazon SES.
+    recipient_email = os.environ[
+        "RECIPIENT"
+    ]  # If your account is still in the sandbox, this address must be verified.
 
-    # Replace recipient@example.com with a "To" address. If your account
-    # is still in the sandbox, this address must be verified.
-    RECIPIENT = "pt.chatbot.dev@danstormsconsulting.io"  # TODO env var
+    SENDER = sender_email  # "Sender Name <" + sender_email + ">"
+    RECIPIENT = recipient_email
 
     # Specify a configuration set. If you do not want to use a configuration
     # set, comment the following variable, and the
     # ConfigurationSetName=CONFIGURATION_SET argument below.
-    CONFIGURATION_SET = "ConfigSet"  # TODO could be useful for analytics https://docs.aws.amazon.com/ses/latest/dg/using-configuration-sets.html
+    # CONFIGURATION_SET = "ConfigSet"  # TODO could be useful for analytics https://docs.aws.amazon.com/ses/latest/dg/using-configuration-sets.html
 
     # If necessary, replace us-west-2 with the AWS Region you're using for Amazon SES.
     AWS_REGION = "us-east-1"
@@ -125,22 +128,25 @@ def send_email(contact_info):
     # The email body for recipients with non-HTML email clients.
     BODY_TEXT = (
         "PT-Chatbot Test\r\n"
-        "This email was sent with Amazon SES using the "
-        "AWS SDK for Python (Boto).\r\n"
+        "PT chatbot here! A patient has a question or would like to schedule an appointment "
+        + str(contact_info)
+        + f"""First Name: {contact_info[0]}\r\n
+        !"""
     )
 
     # The HTML body of the email.
-    BODY_HTML = """<html>
+    BODY_HTML = (
+        """<html>
     <head></head>
     <body>
     <h1>PT-Chatbot Test</h1>
-    <p>This email was sent with
-        <a href='https://aws.amazon.com/ses/'>Amazon SES</a> using the
-        <a href='https://aws.amazon.com/sdk-for-python/'>
-        AWS SDK for Python (Boto)</a>.</p>
+    <p>PT chatbot here! A patient has a question or would like to schedule an appointment """
+        + str(contact_info)
+        + """</p>
     </body>
     </html>
                 """
+    )
 
     # The character encoding for the email.
     CHARSET = "UTF-8"
